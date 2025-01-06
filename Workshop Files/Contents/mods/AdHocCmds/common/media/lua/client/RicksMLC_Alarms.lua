4e2d7bcfdd940aa54b03ffb17fb944f95838e0f6
Rick's Mid-Life Crisis <ricksmidlifecrisis@gmail.com>
2025-01-06 14:33:01 +1000

    Updated alarms get nearest building with new code like treasure hunt.

--- Workshop Files/Contents/mods/AdHocCmds/common/media/lua/client/RicksMLC_Alarms.lua
+++ Workshop Files/Contents/mods/AdHocCmds/common/media/lua/client/RicksMLC_Alarms.lua
@@ -4,8 +4,17 @@
 -- TODO: Make multiplayer
 
 RicksMLC_Alarms = {}
+
+-- Note that AmbientStreamManager is not on the server in vanilla.
+-- See function RicksMLC_TreasureHuntMgrServer.OnServerStarted() which instantiates one on the server
+function RicksMLC_Alarms.getNearestBuildingDef(x, y, ...)
+    local metaGrid = getWorld():getMetaGrid() -- Ensure the metaGrid is intitialised before calling AmbientStreamManager.getNearestBuilding()
+    local nearestBuildingDef = AmbientStreamManager.getNearestBuilding(x, y, Vector2f.new())
+    return nearestBuildingDef
+end
+
 function RicksMLC_Alarms.TriggerAlarm()
-    local nearestBuildingDef = AmbientStreamManager.getNearestBuilding(getPlayer():getX(), getPlayer():getY(), Vector2f.new())
+    local nearestBuildingDef = RicksMLC_Alarms.getNearestBuildingDef(getPlayer():getX(), getPlayer():getY())
 
     if not nearestBuildingDef then
         DebugLog.log(DebugType.Mod, "RicksMLC_Alarms.TriggerAlarm() Fail: No nearest building at " .. tostring(getPlayer():getX()) .. "," .. tostring(getPlayer():getY()))
